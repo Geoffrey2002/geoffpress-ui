7a600b2b708e227286136e9f86e7074cf05dba2f
Geoffrey Mwangi <gmwangi3174@gmail.com>
2024-07-03 16:40:26 +0300

    first commit

---
+++ app/(auth)/auth/page.tsx
@@ -0,0 +1,11 @@
+import AuthTabs from '@/components/auth/Authtabs';
+
+const AuthPage = () => {
+  return (
+    <>
+      <AuthTabs />
+    </>
+  );
+};
+
+export default AuthPage;
---
+++ components/Navbar.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "../img/logo.png";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import ThemeToggler from "@/components/ThemeToggler";
+
+const Navbar = () => {
+  return (
+    <div className="bg-primary dark:bg-slate-700 py-2 px-5 flex justify-between text-white">
+      <Link href="/">
+        <Image src={Logo} alt="GeoffreyPress" width={40} />
+      </Link>
+<div className="flex items-center">
+  <ThemeToggler/>
+  <DropdownMenu>
+        <DropdownMenuTrigger className="focus:outline-none">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" alt="@geoffrey" />
+            <AvatarFallback className="text-black">GM</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <Link href="/profile">Profile</Link>
+          </DropdownMenuItem>
+
+          <DropdownMenuItem>
+            <Link href="/auth">Logout</Link>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+</div>
+      
+    </div>
+  );
+};
+
+export default Navbar;
